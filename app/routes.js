c6dd49e782f9a77202293b152c77558ecc0b3a92
Anthony E <anthony.elstob@opencastsoftware.com>
2021-10-11 10:31:18 +0100

    Version 3 new repo

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+const g4s3 = require('./routes/g4s3');;
+
+router.use(g4s3);
 
 module.exports = router
